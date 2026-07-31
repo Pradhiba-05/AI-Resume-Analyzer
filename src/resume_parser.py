450dae3030ddb303a33e04b008f2dc14a19411de
Pradhiba09 <pradhiba0509@gmail.com>
2026-07-31 19:03:33 +0530

    Added PDF resume parser

---
+++ src/resume_parser.py
@@ -0,0 +1,17 @@
+from PyPDF2 import PdfReader
+
+
+def extract_text(pdf_file):
+    """
+    Extract text from an uploaded PDF resume.
+    """
+    reader = PdfReader(pdf_file)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text + "\n"
+
+    return text
